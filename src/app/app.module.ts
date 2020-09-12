200f67e2dd8e87569d1297540bda6f4af62c8d4e
Igor <igor06brw@gmail.com>
2020-09-12 13:01:52 +0200

    Create component of search car

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './header/navigation/navigation.component';
+import { SearchCarComponent } from './search-car/search-car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    SearchCarComponent
   ],
   imports: [
     BrowserModule,
