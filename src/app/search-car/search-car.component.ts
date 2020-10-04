794065085fef0554c904f53c0f32f5210bde478c
Igor <igor06brw@gmail.com>
2020-10-04 03:52:46 +0200

    make arrays of audi & bmw model cars

--- src/app/search-car/search-car.component.ts
+++ src/app/search-car/search-car.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SearchCarComponent implements OnInit {
 
   manufactures: Array<String> = ['Audi', 'BMW', 'Jaguar', 'Mercedes Benz', 'Land Rover', 'Lexus', 'Volvo']
+  audiCars: Array<String> = ['A1', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8'];
+  bmwCars: Array<String> = ['Series 1', 'Series 2','Series 3' ,'Series 4' ,'Series 5' ,'Series 6' ,'Series 7'];
 
   constructor() { }
 
