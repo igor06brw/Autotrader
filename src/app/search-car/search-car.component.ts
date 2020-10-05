05ffb08a6831c3a9f8f31d20f06bd01baff340a4
Igor <igor06brw@gmail.com>
2020-10-05 03:56:05 +0200

    make service

--- src/app/search-car/search-car.component.ts
+++ src/app/search-car/search-car.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CarsService } from '../services/cars.service';
 
 @Component({
   selector: 'app-search-car',
@@ -10,12 +11,11 @@ export class SearchCarComponent implements OnInit {
   manufactures: Array<String> = ['Audi', 'BMW', 'Jaguar', 'Mercedes Benz', 'Land Rover', 'Lexus', 'Volvo']
   audiCars: Array<String> = ['A1', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8'];
   bmwCars: Array<String> = ['Series 1', 'Series 2','Series 3' ,'Series 4' ,'Series 5' ,'Series 6' ,'Series 7'];
+  model: Array<String> = [];
+
 
   @ViewChild('manufacture') manufacture: ElementRef;
-
-
-  
-  constructor() { }
+  constructor(carsService: CarsService) { }
 
 
 
@@ -23,7 +23,7 @@ export class SearchCarComponent implements OnInit {
   }
 
   onManufacture(value: any) {
-    console.log(this.audiCars);
+    
   }
 
 }
--- src/app/services/cars.service.ts
+++ src/app/services/cars.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarsService {
 
-  constructor(private http: HttpClient) { 
+  model: Array<String> = [];
 
+  constructor() { 
+  }
+
+  choicedManufacture(value: Array<String>) {
+   this.model = value;
   }
 }
