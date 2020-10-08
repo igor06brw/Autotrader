4211ace160935234e8ab82bbebeb8fe0a557c484
Igor <igor06brw@gmail.com>
2020-10-09 01:54:32 +0200

    mileage in separate file

--- src/app/base/mileages.ts
+++ src/app/base/mileages.ts
@@ -1,9 +1,1 @@
-export const MODELS: Array<Object> = [
-    { a3: 'A3', a4: 'A4' },
-    { id: 1, name: 'bmw', title: 'BMW' },
-    { id: 2, name: 'jaguar', title: 'Jaguar' },
-    { id: 3, name: 'mercedesBenz', title: 'Mercedes Benz' },
-    { id: 4, name: 'landRover', title: 'Land Rover' },
-    { id: 5, name: 'lexus', title: 'Lexus' },
-    { id: 6, name: 'volvo', title: 'Volvo' },
-];
+export const MILEAGES: Array<Object> = [ {2500: '2500', 3000: '3000', 4000: '4000', 5000: '5000', 7000: '7000'} ];
