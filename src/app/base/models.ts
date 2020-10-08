8a87cef682e8fa240805d44b846e9224b8b5c0e7
Igor <igor06brw@gmail.com>
2020-10-08 14:16:03 +0200

    arr<object> of models.ts

--- src/app/base/models.ts
+++ src/app/base/models.ts
@@ -1,12 +1,10 @@
 
-const audi: Array<String> = ['A1', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'Q1', 'Q3', 'Q5', 'Q7'];
-const bmw: Array<String> = ['Series 1', 'Series 2','Series 3' ,'Series 4' ,'Series 5' ,'Series 6' ,'Series 7', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7'];
-const volvo: Array<String> = ['S60', 'S90','V60' ,'V90' ,'XC90', 'XC60', 'XC40'];
-const jaguar: Array<String> = ['XF', 'XE','F-Type' ,'I-Pace' ,'F-Pace'];
-const mercedes: Array<String> = ['A Class', 'B Class','C Class' ,'CLS' ,'E Class' ,'S Class', 'GLA', 'GLB', 'GLC', 'GLE', 'GLS'];
-const lexus: Array<String> = ['UX', 'NX','RX' ,'IS' ,'LS', 'CT'];
-const landrover: Array<String> = ['Range Rover', 'Range Rover Sport','Range Rover Velar' ,'Range Rover Evoque' ,'Discovery', 'Defender'];
-export const allModelsOfCars = [audi, bmw, jaguar, mercedes, volvo, lexus, landrover]
-// Array<String> = ['A1', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8'];
-
-// bmw: Array<String> = ['Series 1', 'Series 2','Series 3' ,'Series 4' ,'Series 5' ,'Series 6' ,'Series 7'];
+export const models: Array<Object> = [
+    {id: 0, name: 'audi', model: ['A1', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'Q1', 'Q3', 'Q5', 'Q7']},
+    {id: 1, name: 'bmw', model: ['Series 1', 'Series 2','Series 3' ,'Series 4' ,'Series 5' ,'Series 6' ,'Series 7', 'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7']},
+    {id: 2, name: 'volvo', model: ['S60', 'S90','V60' ,'V90' ,'XC90', 'XC60', 'XC40']},
+    {id: 3, name: 'jaguar', model: ['XF', 'XE','F-Type' ,'I-Pace' ,'F-Pace']},
+    {id: 4, name: 'mercedes', model: ['A Class', 'B Class','C Class' ,'CLS' ,'E Class' ,'S Class', 'GLA', 'GLB', 'GLC', 'GLE', 'GLS']},
+    {id: 5, name: 'lexus', model: ['UX', 'NX','RX' ,'IS' ,'LS', 'CT']},
+    {id: 6, name: 'landrover', model: ['Range Rover', 'Range Rover Sport','Range Rover Velar' ,'Range Rover Evoque' ,'Discovery', 'Defender']},
+]
