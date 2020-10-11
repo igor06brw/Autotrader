a7950e5202fbf217734e02cd597aea2ebf876e72
Igor <igor06brw@gmail.com>
2020-10-12 00:10:02 +0200

    add detail component, fix route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CarComponent } from './car/car.component';
+import { CarDetailComponent } from './car/car-detail/car-detail.component';
 import { SearchCarComponent } from './search-car/search-car.component';
 
 const routes: Routes = [
   { path: '', component: SearchCarComponent },
-  { path: 'car/:id', component: CarComponent }
+  { path: 'car-detail/:id', component: CarDetailComponent }
 ];
 
 @NgModule({
