16c1d4899323138a6f8e428fe702a6774d098266
Igor <igor06brw@gmail.com>
2020-10-26 19:39:15 +0100

    ngFor of cars display on template

--- src/app/models/cars.collection.ts
+++ src/app/models/cars.collection.ts
@@ -47,7 +47,7 @@ const car4: Car = {
     year: 2019, 
     mileage: 9876,
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    imageMain: '',
+    imageMain: '../../assets/landrover/common/landrover-evoque-1.jpg',
     imageFront: '',
     imageBack: '', 
     imageThumbnail: '../../assets/landrover/thumbnails/landrover_thumbnail_1.jpg'
