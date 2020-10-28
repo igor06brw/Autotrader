d38faea013224bb06fdc161a9468570cfe438298
Igor <igor06brw@gmail.com>
2020-10-28 02:39:13 +0100

    make carousel of random-cars-two

--- src/app/random-car-two/random-car-two.component.ts
+++ src/app/random-car-two/random-car-two.component.ts
@@ -9,10 +9,20 @@ import { CAR_COLLECTION } from '../models/cars.collection';
 export class RandomCarTwoComponent implements OnInit {
 
   cars: any = CAR_COLLECTION;
+  galleryCars: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.galleryCars = this.chunk(this.cars, 7);
+  }
+
+  chunk(arr: any, chunkSize:any) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
   }
 
 }
